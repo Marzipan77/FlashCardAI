ed21a92f4434e661873bafa0d24c8f1fda251871
Marzipan <marcis.darguzs9@gmail.com>
2025-06-05 16:30:46 +0300

    Create main.py
     Virtual environment is activated (venv/)
    
    FastAPI is running on http://127.0.0.1:8000

---
+++ server/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+import fitz  # PyMuPDF
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/upload-pdf/")
+async def upload_pdf(file: UploadFile = File(...)):
+    contents = await file.read()
+    doc = fitz.open(stream=contents, filetype="pdf")
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    return {"text": text[:2000]}
+    
